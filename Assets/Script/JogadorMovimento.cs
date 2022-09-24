ce61c74f9b8b3feee789d1e52092cb354b41f70e
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-09-24 12:24:12 -0300

    Update JogadorMovimento.cs

--- Assets/Script/JogadorMovimento.cs
+++ Assets/Script/JogadorMovimento.cs
@@ -9,16 +9,11 @@ public class ControlaJogador : MonoBehaviour
     private float eixoX;
     private float eixoZ;
     private Vector3 direcao;
-    public LayerMask MascaraObjeto; // definir novo layer para objeto selecionado
-    public GameObject TextoGameOver;
-    public bool Vivo = true;
     private Rigidbody rigidbodyJogador;
-    private Animator animatorJogador;
 
     void Start()
     {
         rigidbodyJogador = GetComponent<Rigidbody>();
-        animatorJogador = GetComponent<Animator>();
     }
 
     void Update()
@@ -35,26 +30,5 @@ public class ControlaJogador : MonoBehaviour
         rigidbodyJogador.MovePosition
             (rigidbodyJogador.position +
             (direcao * Velocidade * Time.deltaTime));
-
-        //rotacao da camera
-        //variavel do tipo raio para identificar posicao
-        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //mostra raio
-        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
-
-        //Variavel que pega posicao que o raio tem impacto com um objeto
-        RaycastHit impacto;
-        //Testa a fisica do. gera raio(raio gerado, variavel de impacto, limite de distancia do raio, restricao de objeto que a mascara acerta)
-        if (Physics.Raycast(raio, out impacto, 100, MascaraObjeto))
-        {               //salva posicao do raio a partir da posicao do jogador
-            Vector3 posicaoMiraJogador = impacto.point - transform.position;
-            //fixa o y da rotacao
-            posicaoMiraJogador.y = transform.position.y;
-            //valor posicao rotacao
-            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
-            //altera rotacao do jogador
-            rigidbodyJogador.MoveRotation(novaRotacao);
-
-        }
     }
 }
